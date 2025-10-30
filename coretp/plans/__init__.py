6a80640e5b990dfa57ef936f98a57ee969a7a08b
Nick Matus <nmatus@tenstorrent.com>
2025-10-30 12:46:00 -0500

    removing unfinished svadu plans until finished

--- coretp/plans/__init__.py
+++ coretp/plans/__init__.py
@@ -7,7 +7,6 @@ from .test_plan_registry import new_test_plan, get_plan, list_plans, query_plans
 
 # Have to import all plans here to ensure they are registered. Need to include the module itself to ensure it's registered.
 from .paging import page_table_walks
-from .svadu import svadu_scenarios
 from .sstc import sstc_scenarios
 from .zicond import zicond_scenarios
 from .zkt import zkt_scenarios
