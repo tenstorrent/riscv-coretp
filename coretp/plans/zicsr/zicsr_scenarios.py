7c629b438187b43a6f456c1035daafd28b85dd02
Nicholas Joaquin <njoaquin@ausc-rv-l-13.local.tenstorrent.com>
2026-01-30 17:31:54 -0600

    Lint and change csrr src1 to li's

--- coretp/plans/zicsr/zicsr_scenarios.py
+++ coretp/plans/zicsr/zicsr_scenarios.py
@@ -35,16 +35,17 @@ def SID_ZICSR_01():
     Grab a random CSR and perform an atomic SWAP with 0xFFFFF
     Ensure that CSR matches privilege mode. Do direct accesses only
     """
-    comment = Comment(comment="CSRRW: Atomic swap with 0xFFFFF, rd is non-zero")
+    comment = Comment(comment="CSRRW: Atomic swap with max-int, rd is non-zero")
 
-    csr_access = CsrAccess(op="csrrw", csr_name=None, direct_access=True, src1=0xFFFFF, target_is_x0=False)
+    li_src = LoadImmediateStep(imm=-1)
+    csr_access = CsrAccess(op="csrrw", csr_name=None, direct_access=True, src1=li_src, target_is_x0=False)
 
     return TestScenario.from_steps(
         id="1",
         name="SID_ZICSR_01",
         description="CSRRW - Set to WARL bits, rd is non zero",
         env=TestEnvCfg(),
-        steps=[comment, csr_access],
+        steps=[comment, li_src, csr_access],
     )
 
 
@@ -55,16 +56,17 @@ def SID_ZICSR_02():
     Grab a random CSR and perform an atomic SWAP with 0xFFFFF. Set rd to x0 to prevent read
     Ensure that CSR matches privilege mode. Do direct accesses only
     """
-    comment = Comment(comment="CSRRW: Atomic swap with 0xFFFFF, rd is x0 (no read)")
+    comment = Comment(comment="CSRRW: Atomic swap with max-int, rd is x0 (no read)")
 
-    csr_access = CsrAccess(op="csrrw", csr_name=None, direct_access=True, src1=0xFFFFF, target_is_x0=True)
+    li_src = LoadImmediateStep(imm=-1)
+    csr_access = CsrAccess(op="csrrw", csr_name=None, direct_access=True, src1=li_src, target_is_x0=True)
 
     return TestScenario.from_steps(
         id="2",
         name="SID_ZICSR_02",
         description="CSRRW - Set to WARL bits, rd is zero",
         env=TestEnvCfg(),
-        steps=[comment, csr_access],
+        steps=[comment, li_src, csr_access],
     )
 
 
@@ -95,16 +97,17 @@ def SID_ZICSR_04():
     Grab a random CSR and set all bits by setting mask to all Fs
     Ensure that CSR matches privilege mode. Do direct accesses only
     """
-    comment = Comment(comment="CSRRS: Set all bits using mask 0xFFFFF")
+    comment = Comment(comment="CSRRS: Set all bits using max-int mask")
 
-    csr_access = CsrAccess(op="csrrs", csr_name=None, direct_access=True, src1=0xFFFFF, target_is_x0=False)
+    li_src = LoadImmediateStep(imm=-1)
+    csr_access = CsrAccess(op="csrrs", csr_name=None, direct_access=True, src1=li_src, target_is_x0=False)
 
     return TestScenario.from_steps(
         id="4",
         name="SID_ZICSR_04",
         description="CSRRS - Set all bits",
         env=TestEnvCfg(),
-        steps=[comment, csr_access],
+        steps=[comment, li_src, csr_access],
     )
 
 
@@ -135,16 +138,17 @@ def SID_ZICSR_06():
     Grab a random CSR and clear all bits by setting mask to all Fs
     Ensure that CSR matches privilege mode. Do direct accesses only
     """
-    comment = Comment(comment="CSRRC: Clear all bits using mask 0xFFFFF")
+    comment = Comment(comment="CSRRC: Clear all bits using max-int mask")
 
-    csr_access = CsrAccess(op="csrrc", csr_name=None, direct_access=True, src1=0xFFFFF, target_is_x0=False)
+    li_src = LoadImmediateStep(imm=-1)
+    csr_access = CsrAccess(op="csrrc", csr_name=None, direct_access=True, src1=li_src, target_is_x0=False)
 
     return TestScenario.from_steps(
         id="6",
         name="SID_ZICSR_06",
         description="CSRRC - Clear all bits",
         env=TestEnvCfg(),
-        steps=[comment, csr_access],
+        steps=[comment, li_src, csr_access],
     )
 
 
@@ -364,12 +368,18 @@ def SID_ZICSR_16():
         env=TestEnvCfg(),
         steps=[
             comment,
-            arith_1, csrrw,
-            arith_2, csrrs,
-            arith_3, csrrc,
-            arith_4, csrrwi,
-            arith_5, csrrsi,
-            arith_6, csrrci,
+            arith_1,
+            csrrw,
+            arith_2,
+            csrrs,
+            arith_3,
+            csrrc,
+            arith_4,
+            csrrwi,
+            arith_5,
+            csrrsi,
+            arith_6,
+            csrrci,
             arith_7,
         ],
     )
