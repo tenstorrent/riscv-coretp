d6e5d13e7879e9dd556488b40201a21752f2ca60
Lisa Elmiladi <lelmiladi@ausc-rv-l-13.local.tenstorrent.com>
2026-02-05 15:35:49 -0600

    Added added the import of the zihintntl_scenarios module to the main    plans/__init__.py

--- coretp/plans/__init__.py
+++ coretp/plans/__init__.py
@@ -22,5 +22,6 @@ from .sscofpmf import sscofpmf_scenarios
 from .hypervisor import hypervisor_scenarios
 from .ssu64xl import ssu64xl_scenarios
 from .smstateen_ssstateen import smstateen_ssstateen_scenarios
+from .zihintntl import zihintntl_scenarios
 
 __all__ = ["new_test_plan", "get_plan", "list_plans", "query_plans"]
